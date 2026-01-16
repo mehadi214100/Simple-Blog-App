74ebaa20f0f7e4d979d43a692a0c91dee852c96c
mehadi <mehadi214100@gmail.com>
2026-01-17 01:37:44 +0600

    added categories option

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("",views.home,name="home"),
+    path("add_category/",views.add_category,name="add_category"),
+    path("delete_category/<int:id>/",views.delete_category,name="delete_category"),
+    path("edit_category/<int:id>/",views.edit_category,name="edit_category"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Category
+from .forms import categoryForm
 
-# Create your views here.
+def home(request):
+    return render(request,"index.html")
+
+def add_category(request):
+
+    categories = Category.objects.all()
+    total = categories.count()
+    
+    context = {
+        "categories":categories,
+        "total":total
+    }
+
+    if(request.method == 'POST'):
+        form = categoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('add_category')
+        
+    else:
+        form = categoryForm()
+
+    return render(request,"add-category.html",context=context)
+
+def delete_category(request,id):
+    cat = Category.objects.get(id=id)
+    cat.delete()
+    return redirect('add_category')
+
+
+def edit_category(request,id):
+    cat = Category.objects.get(id=id)
+
+    if(request.method == 'POST'):
+        form = categoryForm(request.POST,instance=cat)
+
+        if form.is_valid():
+            form.save()
+            return redirect('add_category')
+
+    categories = Category.objects.all() 
+    
+    return render(request, "add-category.html", {'cat':cat,'total': categories.count(), 'categories': categories})
